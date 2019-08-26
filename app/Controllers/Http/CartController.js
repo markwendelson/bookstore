003d565e3d424ac06d4e4105e5045f2196ddc1cd
Mark Wendelson R. Olog <markwendelson@gmail.com>
2019-08-26 16:44:44 +0800

    update

--- app/Controllers/Http/CartController.js
+++ app/Controllers/Http/CartController.js
@@ -57,6 +57,16 @@ class CartController {
             'quantity'
         ])
 
+        // check books quantity if has stocks
+        const book = await Books.find(book_id)
+        if (book.quantity == 0) {
+            return response.json({
+                message: "No more available stock.",
+                status: 'error',
+                data: null
+            });
+        }
+
         const user_id = auth.user.id
 
         const cart = await Cart.create({ 
@@ -66,7 +76,6 @@ class CartController {
         })
 
         // update book quantity
-        const book = await Books.find(book_id)
         book.quantity = book.quantity - quantity
         await book.save()
 
@@ -79,6 +88,11 @@ class CartController {
 
     async destroy ({ params, response }) {
         const cart = await Cart.find(params.id)
+
+        // update books quantity
+        const books = await Books.find(cart.book_id)
+        books.quantity = books.quantity + cart.quantity
+        await books.save()
         
         await cart.delete()
 
@@ -91,37 +105,44 @@ class CartController {
 
     async update ({ params, request, response, session }) {
         const cart = await Cart.find(params.id)
-       
-        if (!cart) {
-            return response.status(404).json(null)
-        }
-
         const rules = {
             quantity: 'required'
         }
 
         const validation = await validate(request.all(), rules)
-
+        
         if (validation.fails()) {
             session
-              .withErrors(validation.messages())
-      
+            .withErrors(validation.messages())
+            
             return response.json({
                 message: validation.messages(),
                 status: 200,
                 data: null
             });
         }
-
+        
         const cartInfo = request.only(['quantity'])
 
+        const book = await Books.find(cart.book_id)
+        // check if enough quantity
+        if (book.quantity + cart.quantity < cartInfo.quantity) {
+            return response.json({
+                message: "No more available stock.",
+                status: 'error',
+                data: null
+            });
+        }
+        // update book quantity
+        book.quantity = book.quantity + cart.quantity - cartInfo.quantity
+        await book.save()
+
         cart.quantity = cartInfo.quantity
-       
         await cart.save()
 
         return response.json({
-            message: "cart updated",
-            status: 200,
+            message: "Cart has been updated.",
+            status: 'success',
             data: cart
         });
     
